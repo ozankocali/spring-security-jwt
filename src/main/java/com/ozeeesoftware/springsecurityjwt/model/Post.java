88b0c240a7312b1832f521d356a7343e94fe462e
ozankocali <ozan.kocali@hotmail.com>
2021-05-19 17:39:27 +0300

    Models modified

--- src/main/java/com/ozeeesoftware/springsecurityjwt/model/Post.java
+++ src/main/java/com/ozeeesoftware/springsecurityjwt/model/Post.java
@@ -1,9 +1,19 @@
 package com.ozeeesoftware.springsecurityjwt.model;
 
+import com.ozeeesoftware.springsecurityjwt.constant.PostStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
 import javax.persistence.*;
 
 @Entity
 @Table(name = "posts")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class Post {
 
     @Id
@@ -16,65 +26,5 @@ public class Post {
     @Enumerated(EnumType.STRING)
     private PostStatus postStatus;
 
-    public Post() {
-    }
 
-    public Post(long postId, String subject, String description, String postUserName, PostStatus postStatus) {
-        this.postId = postId;
-        this.subject = subject;
-        this.description = description;
-        this.postUserName = postUserName;
-        this.postStatus = postStatus;
-    }
-
-    public long getPostId() {
-        return postId;
-    }
-
-    public void setPostId(long postId) {
-        this.postId = postId;
-    }
-
-    public String getSubject() {
-        return subject;
-    }
-
-    public void setSubject(String subject) {
-        this.subject = subject;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getPostUserName() {
-        return postUserName;
-    }
-
-    public void setPostUserName(String postUserName) {
-        this.postUserName = postUserName;
-    }
-
-    public PostStatus getPostStatus() {
-        return postStatus;
-    }
-
-    public void setPostStatus(PostStatus postStatus) {
-        this.postStatus = postStatus;
-    }
-
-    @Override
-    public String toString() {
-        return "Post{" +
-                "postId=" + postId +
-                ", subject='" + subject + '\'' +
-                ", description='" + description + '\'' +
-                ", postUserName='" + postUserName + '\'' +
-                ", postStatus=" + postStatus +
-                '}';
-    }
 }
