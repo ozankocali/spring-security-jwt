7f76402dad7d362a2cc3050c26070ef08cd06d0e
ozankocali <ozan.kocali@hotmail.com>
2021-05-19 17:41:06 +0300

    Repository modified

--- src/main/java/com/ozeeesoftware/springsecurityjwt/repository/UserRepository.java
+++ src/main/java/com/ozeeesoftware/springsecurityjwt/repository/UserRepository.java
@@ -4,13 +4,11 @@ import com.ozeeesoftware.springsecurityjwt.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
-    Optional<User> findByUsername(String username);
+    User findUserByUsername(String username);
 
-    Boolean existsByUsername(String username);
+    User findUserByEmail(String email);
 
-    Boolean existsByEmail(String email);
 }
