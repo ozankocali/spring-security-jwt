12b1e930ec07f667396f506e12ffe69569943897
ozankocali <ozan.kocali@hotmail.com>
2021-06-15 22:17:37 +0300

    Repository annotation added to post repository

--- src/main/java/com/ozeeesoftware/springsecurityjwt/repository/PostRepository.java
+++ src/main/java/com/ozeeesoftware/springsecurityjwt/repository/PostRepository.java
@@ -2,6 +2,8 @@ package com.ozeeesoftware.springsecurityjwt.repository;
 
 import com.ozeeesoftware.springsecurityjwt.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PostRepository extends JpaRepository<Post,Long> {
 }
